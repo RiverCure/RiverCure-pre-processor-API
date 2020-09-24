1ec184a2160b8d2f56f317f930aa1b69e00bf97a
jmsmarques <jorgemsmarques9@gmail.com>
2020-09-24 17:02:25 +0100

    Now processing contexts given an id

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, requests
 from flask import Flask, Response
 
 app = Flask(__name__)
@@ -9,11 +9,20 @@ def ping():
     status_code = Response(status=200)
     return status_code
 
-@app.route('/process/')
-def run_pre_processor():
-    os.system('./mesh && \\n')
-    app.logger.warning('User requested a simulation\n')
-    return 'Processed\n'
+@app.route('/process/<string:context_id>')
+def run_pre_processor(context_id):
+    # Request and download files
+    url = f'http://localhost:8000/contexts/download/{context_id}'
+    downloaded_folder = 'download_context.zip'
+    destination_folder = 'simulation/context_files'
+    req = requests.get(url)
+    open(downloaded_folder, 'wb').write(req.content)
+    os.system(f'''rm {destination_folder}/* ; \
+                unzip {downloaded_folder} -d {destination_folder} && rm -R {downloaded_folder} && \
+                (cd {destination_folder} && rename \'s/Coura_*//\' *) && \
+                (cd simulation && ./mesh &)''')
+
+    return 'Files downloaded\n'
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("DEBUG", False)
