1a24ae0a468a8a46d45b91a6b5d1006d626131ea
jmsmarques <jorgemsmarques9@gmail.com>
2020-10-05 18:39:10 +0100

    Paraview Scripts changed

---
+++ paraview_visualizer/paraview-visualizer.sh
@@ -0,0 +1,5 @@
+cd ~/Desktop/ParaView-5.8.1-MPI-Linux-Python3.7-64bit
+./bin/pvpython ./share/paraview-5.8/web/visualizer/server/pvw-visualizer.py \
+--content ./share/paraview-5.8/web/visualizer/www/ \
+--data ~/Desktop/RiverCure-pre-processor-API/paraview_visualizer/data/ \
+--port 8080
