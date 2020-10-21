5b288f5460cfc96a6b310bf5022d6b49b784a2fe
jmsmarques <jorgemsmarques9@gmail.com>
2020-10-21 14:58:32 +0100

    added result download functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os, requests, subprocess
-from flask import Flask, Response, request
+import os, requests, subprocess, zipfile
+from flask import Flask, Response, request, send_file
 
 app = Flask(__name__)
 
@@ -51,12 +51,41 @@ def simulate():
                     request.files[key].save(f'{sensor_data_destination_folder}/{key}')
 
         subprocess.call(f'''(cd simulation && ./solver2D-OMP.dat && cd .. &)''', shell=True)
-
     except Exception as e:
         print(f'Failed simulation!\nException{e}')
         return 'Simulation failed'
 
     return 'Simulation started\n'
+
+@app.route('/simulation/results/')
+def simulation_results():
+    #event id necessary in the future
+    context_name = request.args.get('context_name')
+    event_id = request.args.get('event_id')
+    folder_name = f'{context_name}_event_{event_id}_results.zip'
+
+    os.system(f'''rm -r *.zip || rm -r results/* \
+                ; vtk=$(cd simulation/output/maxima && ls | grep *.vtk) \
+                && (cd simulation/output/rasters && python stavResults.py -i ../maxima/$vtk -o ../../../results/{context_name}_event_{event_id})''')
+    # files = {
+    #     'max_depth': open('out-Max_Depth.tif', 'rb'),
+    #     'max_level': open('out-Max_Level.tif', 'rb'),
+    #     'max_q': open('out-Max_Q.tif', 'rb'),
+    #     'max_vel': open('out-Max_Vel.tif', 'rb')
+    # }
+
+    zipfolder = zipfile.ZipFile(folder_name, 'w', compression = zipfile.ZIP_STORED)
+
+    for root,dirs, files in os.walk('results/'):
+        for file in files:
+            zipfolder.write('results/'+file)
+    zipfolder.close()
+
+    return send_file(folder_name,
+            mimetype = 'zip',
+            attachment_filename= folder_name,
+            as_attachment = True)
+    # return send_file('./out-Max_Depth.tif', attachment_filename='out-Max_Depth.tif')
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("DEBUG", False)
